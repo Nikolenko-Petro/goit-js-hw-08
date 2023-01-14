091c0052b37474400f3b970d48300ef350a41957
Nikolenko-Petro <petya.dnipro@ukr.net>
2023-01-14 16:23:46 +0200

    qwe

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+// import Player from "@vimeo/player";
+// import throttle from "lodash.throttle";
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on(
-  "timeupdate",
-  throttle((e) => {
-    localStorage.setItem("videoplayer-current-time", e.seconds);
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
   }, 1000)
 );
 
 const timeVideo = player.setCurrentTime(
-  localStorage.getItem("videoplayer-current-time")
+  localStorage.getItem('videoplayer-current-time')
 );
 
 timeVideo.catch(function (error) {
