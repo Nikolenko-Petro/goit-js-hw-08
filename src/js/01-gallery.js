275933e1a3ad9befe74dbd925ce46fdf4551213a
Nikolenko-Petro <petya.dnipro@ukr.net>
2023-01-14 16:27:54 +0200

    save lib

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,8 @@
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const items = [];
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,32 @@
-// import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 
-const localStorageKey = "feedback-form-state";
+const localStorageKey = 'feedback-form-state';
 
 form.addEventListener(
-  "input",
-  throttle((e) => {
+  'input',
+  throttle(e => {
     const objectToSave = { email: email.value, message: message.value };
     localStorage.setItem(localStorageKey, JSON.stringify(objectToSave));
   }, 500)
 );
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   console.log({ email: email.value, message: message.value });
   form.reset();
   localStorage.removeItem(localStorageKey);
 });
 
-const load = (key) => {
+const load = key => {
   try {
     const serializedState = localStorage.getItem(key);
     return serializedState === null ? undefined : JSON.parse(serializedState);
   } catch (error) {
-    console.error("Get state error: ", error.message);
+    console.error('Get state error: ', error.message);
   }
 };
 
