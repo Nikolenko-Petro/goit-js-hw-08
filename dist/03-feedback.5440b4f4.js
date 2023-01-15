1c5c399c82f21916a21946508f8dc57e7fe23fa9
Nikolenko-Petro <petya.dnipro@ukr.net>
2023-01-15 13:49:53 +0200

    fix submit

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -509,23 +509,26 @@ var _lodashThrottleDefault = parcelHelpers.interopDefault(_lodashThrottle);
 const form = document.querySelector(".feedback-form");
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
-const localStorageKey = "feedback-form-state";
+const LOCALSTORAGE_KEY = "feedback-form-state";
 form.addEventListener("input", (0, _lodashThrottleDefault.default)((e)=>{
     const objectToSave = {
         email: email.value,
         message: message.value
     };
-    localStorage.setItem(localStorageKey, JSON.stringify(objectToSave));
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
 }, 500));
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    console.log({
+function handleSubmit(event) {
+    event.preventDefault();
+    if (email.value === "" || message.value === "") return alert("Please fill in all the fields!");
+    const userDetails = {
         email: email.value,
         message: message.value
-    });
-    form.reset();
-    localStorage.removeItem(localStorageKey);
-});
+    };
+    console.log(userDetails);
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+form.addEventListener("submit", handleSubmit);
 const load = (key)=>{
     try {
         const serializedState = localStorage.getItem(key);
@@ -534,7 +537,7 @@ const load = (key)=>{
         console.error("Get state error: ", error.message);
     }
 };
-const storageData = load(localStorageKey);
+const storageData = load(LOCALSTORAGE_KEY);
 if (storageData) {
     email.value = storageData.email;
     message.value = storageData.message;
